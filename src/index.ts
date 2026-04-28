cd5a010608c61eb3622d4541b50a1b20746a2a22
pyronaur <hi@pyronaur.com>
2026-04-28 12:11:48 +0300

    remove scratch

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
 
 import { registerPlayground } from "./app/register-playground.ts";
 
-const intentionalGateFailure = true;
-
 export default function register(pi: ExtensionAPI) {
 	registerPlayground(pi);
 }
